e7af0a5f028f51f612c8c7447dd5d5c2e2ce88e9
233 <aowuaoaowu@gmail.com>
2026-03-26 21:52:21 +0800

    chore: set default github app id

--- src/consts.ts
+++ src/consts.ts
@@ -11,6 +11,6 @@ export const GITHUB_CONFIG = {
 	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'Aaowu',
 	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || 'oorz-blog',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
-	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '-',
+	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '3192750',
 	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
 } as const
